07697a524ce27a578e36ca977f93809345ddec18
Bastian Bräu <bastian@braeu.me>
2015-07-24 17:30:10 +0200

    Set editor styles

--- public/wp-content/themes/dgob/functions.php
+++ public/wp-content/themes/dgob/functions.php
@@ -10,3 +10,15 @@ require_once( __DIR__ . '/vendor/wp-bootstrap-navwalker/wp_bootstrap_navwalker.p
  */
 register_nav_menu( 'primary', 'Haupt-Navigation' );
 register_nav_menu( 'footer', 'Footer-Navigation' );
+
+/**
+ * Theme setup
+ */
+function dgob_theme_setup() {
+	add_editor_style( array(
+		'http://fonts.googleapis.com/css?family=Source+Sans+Pro:400&#44;600&#44;700',
+		'style.css'
+	) );
+}
+
+add_action( 'after_setup_theme', 'dgob_theme_setup' );
