242b5ecac974d1e84b5bbaf182d84ad2f765db45
Bastian Bräu <bastian@braeu.me>
2015-07-31 23:40:13 +0200

    Use flexbox for sticky footer

--- public/wp-content/themes/dgob/footer.php
+++ public/wp-content/themes/dgob/footer.php
@@ -1,14 +1,11 @@
-		</div>
 	</div>
-
-	<footer class="page">
-		<div class="footer-line"></div>
-		<div class="container-fluid">
-			<?php dgob_footer(); ?>
-		</div>
-	</footer>
-
 </div>
+
+<footer class="page">
+	<div class="container-fluid">
+		<?php dgob_footer(); ?>
+	</div>
+</footer>
 
 <script src="<?php dgob_timestamped_url( 'scripts.js' ); ?>"></script>
 
--- public/wp-content/themes/dgob/header.php
+++ public/wp-content/themes/dgob/header.php
@@ -12,45 +12,43 @@
 
 <body <?php body_class(); ?>>
 
-<div id="page-wrapper">
-
-	<header class="page">
-		<div class="container-fluid clearfix visible-sm-block visible-md-block visible-lg-block">
-			<form class="search pull-right" method="GET" action="/">
-				<div class="input-group">
-					<input type="text" class="form-control" name="s" placeholder="Suche">
+<header class="page">
+	<div class="container-fluid clearfix visible-sm-block visible-md-block visible-lg-block">
+		<form class="search pull-right" method="GET" action="/">
+			<div class="input-group">
+				<input type="text" class="form-control" name="s" placeholder="Suche">
 					<span class="input-group-addon">
 						<button type="submit">
 							<span class="glyphicon glyphicon-search"></span>
 						</button>
 					</span>
-				</div>
-			</form>
-			<div class="logo">
-				<a href="/">
-					<img src="<?php dgob_timestamped_url( 'images/logo.svg' ); ?>" alt="Logo des Deutschen Go-Bunds">
-				</a>
 			</div>
-			<div class="dgob">Deutscher Go-Bund</div>
+		</form>
+		<div class="logo">
+			<a href="/">
+				<img src="<?php dgob_timestamped_url( 'images/logo.svg' ); ?>" width="100" height="68" alt="Logo des Deutschen Go-Bunds">
+			</a>
 		</div>
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-							data-target="#nav-main-collapse" aria-expanded="false">
-						<span class="sr-only">Navigation umschalten</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a href="/" class="navbar-brand visible-xs-inline">Deutscher Go-Bund</a>
-				</div>
-				<div id="nav-main-collapse" class="collapse navbar-collapse">
-					<?php dgob_main_menu(); ?>
-				</div>
-			</div>
-		</nav>
-	</header>
-
-	<div id="main-content">
+		<div class="dgob">Deutscher Go-Bund</div>
+	</div>
+	<nav class="navbar navbar-default">
 		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+				        data-target="#nav-main-collapse" aria-expanded="false">
+					<span class="sr-only">Navigation umschalten</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="/" class="navbar-brand visible-xs-inline">Deutscher Go-Bund</a>
+			</div>
+			<div id="nav-main-collapse" class="collapse navbar-collapse">
+				<?php dgob_main_menu(); ?>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<div id="main-content">
+	<div class="container-fluid">
