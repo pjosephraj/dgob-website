a16f1bf748bf9e2d763525a0f11dcbb60d5c4054
Bastian Bräu <bastian@braeu.me>
2015-08-09 11:40:38 +0200

    Add SMTPSecure option

--- public/wp-content/plugins/dgob-smtp/dgob-smtp.php
+++ public/wp-content/plugins/dgob-smtp/dgob-smtp.php
@@ -7,10 +7,11 @@
 add_action( 'phpmailer_init', function ( PHPMailer $phpmailer ) {
 	if ( defined( 'DGOB_SMTP' ) && true === DGOB_SMTP ) {
 		$phpmailer->IsSMTP();
-		$phpmailer->Host     = DGOB_SMTP_HOST;
-		$phpmailer->Port     = DGOB_SMTP_PORT;
-		$phpmailer->SMTPAuth = DGOB_SMTP_AUTH;
-		$phpmailer->Username = DGOB_SMTP_USER;
-		$phpmailer->Password = DGOB_SMTP_PASS;
+		$phpmailer->Host       = DGOB_SMTP_HOST;
+		$phpmailer->Port       = DGOB_SMTP_PORT;
+		$phpmailer->SMTPSecure = DGOB_SMTP_SECURE;
+		$phpmailer->SMTPAuth   = DGOB_SMTP_AUTH;
+		$phpmailer->Username   = DGOB_SMTP_USER;
+		$phpmailer->Password   = DGOB_SMTP_PASS;
 	}
 } );
